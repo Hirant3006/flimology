5d414109ff763d27a335fbd80aadb9fb00b52628
15110091 <lychautrinha@gmail.com>
2019-07-21 22:09:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <movie-tag msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import MovieTag from './components/MovieTag.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MovieTag
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/MovieTag.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'MovieTag',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
